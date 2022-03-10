ce008a4fb7bf1556e1e63b9db1c223aef73d211d
Nathan <nathanfield32@gmail.com>
2022-03-10 00:53:03 +0000

    updated maps, implemented priority queue and menu option, sort type, added wins field and comparator.

---
+++ src/main/java/org/example/ChampionshipWinsComparator.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Comparator;
+
+public class ChampionshipWinsComparator implements Comparator<Player> {
+
+    private SortType sortType;
+
+    public ChampionshipWinsComparator(SortType sortType)
+    {
+        this.sortType = sortType;
+    }
+    @Override
+    public int compare(Player p1, Player p2) {
+        int direction = sortType.getValue();
+        return direction * (p1.getChampionshipWins() - p2.getChampionshipWins());
+    }
+
+
+}
--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -11,14 +11,16 @@ public class Player{
         private double weight;
         private double height;
         private LocalDate birthDate;
+        private int championshipWins;
 
-        public Player(int playerID, String firstName, String lastName, double weight, double height, LocalDate birthDate) {
+        public Player(int playerID, String firstName, String lastName, double weight, double height, LocalDate birthDate, int championshipWins) {
                 this.playerID = playerID;
                 this.firstName = firstName;
                 this.lastName = lastName;
                 this.weight = weight;
                 this.height = height;
                 this.birthDate = birthDate;
+                this.championshipWins = championshipWins;
         }
 
         public int getPlayerID() {
@@ -69,29 +71,32 @@ public class Player{
                 this.birthDate = birthDate;
         }
 
+        public int getChampionshipWins() {
+                return championshipWins;
+        }
+
+        public void setChampionshipWins(int championshipWins) {
+                this.championshipWins = championshipWins;
+        }
+
         @Override
         public boolean equals(Object o) {
                 if (this == o) return true;
                 if (o == null || getClass() != o.getClass()) return false;
                 Player player = (Player) o;
-                return playerID == player.playerID && Double.compare(player.weight, weight) == 0 && Double.compare(player.height, height) == 0 && firstName.equals(player.firstName) && lastName.equals(player.lastName) && birthDate.equals(player.birthDate);
+                return playerID == player.playerID && Double.compare(player.weight, weight) == 0 && Double.compare(player.height, height) == 0 && championshipWins == player.championshipWins && firstName.equals(player.firstName) && lastName.equals(player.lastName) && birthDate.equals(player.birthDate);
         }
 
         @Override
         public int hashCode() {
-                return Objects.hash(playerID, firstName, lastName, weight, height, birthDate);
+                return Objects.hash(playerID, firstName, lastName, weight, height, birthDate, championshipWins);
         }
 
         @Override
-        public String toString() {
-                return "Player{" +
-                        "playerID=" + playerID +
-                        ", firstName='" + firstName + '\'' +
-                        ", lastName='" + lastName + '\'' +
-                        ", weight=" + weight +
-                        ", height=" + height +
-                        ", birthDate=" + birthDate +
-                        '}';
+        public String toString()
+        {
+                   return String.format("%-10s%-10s%-10s%-10.2f%-10.2f%-15s%5d\n", playerID, firstName, lastName, weight, height, birthDate, championshipWins);
         }
+
 
 }
